261e4cabbb79c092274de1837e59a1e0ac3296ab
Shani Almog <killerdrum@gmail.com>
2021-06-26 19:27:46 +0300

    update api key

--- src/utils/API.js
+++ src/utils/API.js
@@ -4,7 +4,7 @@ const cityData = require('./mocks/mockDataCity.json')
 const citiesSearchResults = require('./mocks/mockDataSearch.json')
 // const geolocationWeather = require('./mocks/mockDataGeolocation.json')
 
-const API_KEY = 'GOmUaaattNYHbBWGLAoVAwP3oK1QxGLC'
+const API_KEY = '95aNm3IByBwCkB8CTzfaBHAvADSLgy98'
 const BASE_URL = 'http://dataservice.accuweather.com'
 const USE_MOCK_DATA = false
 
@@ -12,12 +12,16 @@ const sleep = ms => new Promise(resolve => {
     setTimeout(resolve, ms)
 })
 
+const fetchOptions = {
+    headers: {}
+}
+
 export const getCityWeather = async (cityKey) => {
     if (USE_MOCK_DATA) {
         await sleep(200)
         return cityWeather
     }
-    const data = await fetch(`${BASE_URL}/currentconditions/v1/${cityKey}/?apikey=${API_KEY}`)
+    const data = await fetch(`${BASE_URL}/currentconditions/v1/${cityKey}/?apikey=${API_KEY}`, fetchOptions)
     return data.json()
 }
 
@@ -25,7 +29,7 @@ export const getCityForecast = async (cityKey) => {
     if (USE_MOCK_DATA) {
         return cityWeatherForecast
     }
-    const data = await fetch(`${BASE_URL}/forecasts/v1/daily/5day/${cityKey}?metric=true&apikey=${API_KEY}`)
+    const data = await fetch(`${BASE_URL}/forecasts/v1/daily/5day/${cityKey}?metric=true&apikey=${API_KEY}`, fetchOptions)
     return data.json()
 }
 
@@ -33,13 +37,13 @@ export const getCityData = async (cityKey) => {
     if (USE_MOCK_DATA) {
         return cityData
     }
-    const data = await fetch(`${BASE_URL}/locations/v1/${cityKey}?apikey=${API_KEY}`)
+    const data = await fetch(`${BASE_URL}/locations/v1/${cityKey}?apikey=${API_KEY}`, fetchOptions)
     return data.json()
 }
 
 export const searchCities = async (query) => {
     // This endpoint doesn't have CORS headers so, using mock data
-    // const data = await fetch(`${BASE_URL}/locations/v1/cities/autocomplete?q=${query}?apikey=${API_KEY}`)
+    // const data = await fetch(`${BASE_URL}/locations/v1/cities/autocomplete?q=${query}?apikey=${API_KEY}`, fetchOptions)
     // return data.json()
     return citiesSearchResults
 }
